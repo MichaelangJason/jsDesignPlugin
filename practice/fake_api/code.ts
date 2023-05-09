f295df7641d3cf2bbd6e36ade02d0ab78843363e
mkljs <mkl.js@outlook.com>
2023-05-09 20:34:58 +0800

    chore(fake_api): creates fake_api project for practicing Promise/await/async and outer API

---
+++ practice/fake_api/code.ts
@@ -0,0 +1,23 @@
+
+jsDesign.showUI(__html__, {
+    width: 260,
+    height: 393,
+})
+
+jsDesign.ui.onmessage = (msg) => {
+    const fakeAPI = 'https://reqres.in/api/users'
+    // fetch creates a Promise object
+    fetch(fakeAPI)
+        .then(res => {
+            if (!res.ok) {
+                console.log("Incorrect")
+                return
+            }
+            console.log("Success")
+            return res.json()
+        }).then(data => {
+            console.log(data.data[2].email)
+        })
+
+    
+}
