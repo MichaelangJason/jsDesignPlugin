d0cfc1935600b07bcc36da60471ec4c4b67e1c75
mkljs <mkl.js@outlook.com>
2023-05-10 14:25:58 +0800

    chore(fake_api): brandfetch API authentication info added

--- practice/fake_api/code.js
+++ practice/fake_api/code.js
@@ -3,18 +3,34 @@ jsDesign.showUI(__html__, {
     width: 260,
     height: 393,
 });
+// API authentication parameters
+const authentication = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer hEBPHU9ZzUiwY5WuhHklAZyhOXodkGCPfRxzNOFZ02o=',
+        //Credential: 'omit',
+        Referer: 'https://js.design'
+    }
+};
+/**
+ * this function only process API request based on user input
+ * any webpage changes are made in GUI file
+ */
 jsDesign.ui.onmessage = (msg) => {
-    const fakeAPI = 'https://reqres.in/api/users';
-    // fetch creates a Promise object
-    fetch(fakeAPI)
-        .then(res => {
-        if (!res.ok) {
-            console.log("Incorrect");
-            return;
-        }
-        console.log("Success");
-        return res.json();
-    }).then(data => {
-        console.log(data.data[2].email);
-    });
+    // user input assume to have none empty string value
+    // combine user input with API access URL
+    const searchInput = 'https://api.brandfetch.io/v2/search/' + msg.val;
+    let brandsInfo;
+    // invoke API to gain the result
+    // fetch(searchInput, authentication)
+    //     .then(res => {
+    //         brandsInfo = res.json()
+    //         return brandsInfo;
+    //     })
+    //     .then(res => console.log(res))
+    //     .catch(err => console.log(err))
+    fetch('https://reqres.in/api/users')
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
 };
